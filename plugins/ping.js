2dec31e6a3b77d8fb755e32bc2541d98a78f5acb
cyber-kiddz <109889758+Diegoson@users.noreply.github.com>
2022-10-13 18:46:42 +0200

    Update ping.js

--- plugins/ping.js
+++ plugins/ping.js
@@ -1,3 +1,11 @@
+const { command ,isPrivate} = require("../lib/");
+
+/* Copyright (C) 2022 Diegoson.
+Licensed under the  GPL-3.0 License;
+you may not use this file except in compliance with the License.
+Amarok - Diegoson
+*/
+
 command(
   {
     pattern: "ping",
