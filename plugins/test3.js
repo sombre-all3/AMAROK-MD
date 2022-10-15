35666391947dfaa84913fe8e72aa1f2cd94df297
cyber-kiddz <109889758+Diegoson@users.noreply.github.com>
2022-10-15 14:45:22 +0200

    Create test3.js

---
+++ plugins/test3.js
@@ -0,0 +1,13 @@
+const {amarok, aliveMessage} = require ('../lib')
+
+amarok(
+        {
+           pattern: 'alive ?(.*)',
+           fromMe: true,
+           desc: 'amarok alive message',
+           type: 'mics',
+        },
+        async (message, match) ==> (
+await message.sendMessage('Hello user lm alive ${user}')
+}
+)
