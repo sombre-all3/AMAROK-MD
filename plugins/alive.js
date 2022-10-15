6800626348cd67dc8fb7b9b8d763411ccc4634fe
cyber-kiddz <109889758+Diegoson@users.noreply.github.com>
2022-10-15 15:34:49 +0200

    Create alive.js

---
+++ plugins/alive.js
@@ -0,0 +1,18 @@
+const {bot} = require ("../lib")
+
+bot(
+        {
+           pattern: 'alive ?(.*)',
+           fromMe: true,
+           desc: 'amarok alive message',
+           type: 'mics',
+        },
+        async (message,match) => {
+     let user = message.participant || message.mention[0];   	
+        
+await message.sendMessage(`Hello  ${user} all systems are functional`)
+}
+)
+
+
+//Tshephang+Cyber 🐤you change plugin disappeares
