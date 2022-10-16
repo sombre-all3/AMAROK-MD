6e53e50ed0720ef4e7ab08ed147ea255b7032f8b
cyber-kiddz <109889758+Diegoson@users.noreply.github.com>
2022-10-16 19:28:26 +0200

    Fixed

--- plugins/eval.js
+++ plugins/eval.js
@@ -38,7 +38,7 @@ const {
 const util = require("util");
 const config = require("../config");
 
-command({pattern:'eval', on: "text", fromMe: true,desc :'Runs a server code'}, async (message, match, m, client) => {
+command({pattern:"eval", on: "text", fromMe: true,desc :'Runs a server code'}, async (message, match, m, client) => {
   if (match.startsWith(">")) {
     //const m = message;
     try {
