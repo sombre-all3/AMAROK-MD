ca48ac3a069276a0035381b06f07e8192e7a37e5
cyber-kiddz <109889758+Diegoson@users.noreply.github.com>
2022-10-16 21:47:27 +0200

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -31,7 +31,7 @@ alive+= `
 `
 
 await message.client.sendMessage(message.jid,{
-image: { url: `https://i.imgur.com/w5wr6c1.jpeg` },
+image: { url: `https://i.imgur.com/vwNY4lg.jpeg' },
       caption: alive,
       footer: tiny(`alive.js❤️` ),
 
