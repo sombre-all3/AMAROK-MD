bdbf66b51dbe8393682cba0115bca621a0463f8b
cyber-kiddz <109889758+Diegoson@users.noreply.github.com>
2022-10-16 21:15:39 +0200

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -33,12 +33,12 @@ alive+= `
    }
       buttonld: '${prefix}support group',
       buttonText: {displayText: ("⫷𝗦𝗨𝗣𝗣𝗢𝗥𝗧 𝗚𝗥𝗢𝗨𝗣⫸") },
-    },
-    {
+   },
+   {
       buttonld: '${prefix}github',
       buttonText: {displayText: ("⫷𝗚𝗜𝗧𝗛𝗨𝗕⫸") },
-    },
- }
+   },
+}
 
 await message.client.sendMessage(message.jid,{
 image: { url: `https://i.imgur.com/w5wr6c1.jpeg` },
