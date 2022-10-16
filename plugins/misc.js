183b873d8aefb65b0ffd04940497a59dc452b4cf
cyber-kiddz <109889758+Diegoson@users.noreply.github.com>
2022-10-16 20:33:23 +0200

    𝙑.2.0.0

--- plugins/misc.js
+++ plugins/misc.js
@@ -10,7 +10,7 @@ command(
     for (let index = 0; index < 5; index++) {
       await sleep(30 * 1000);
       await message.sendFromUrl("https://amarok-baileyz.vercel.app/", {
-        caption: "⫷𝙎𝘾𝘼𝙉 𝙒𝙄𝙏𝙃𝙄𝙉 8 𝙈𝙄𝙉𝙐𝙏𝙀𝙎⫸ ",
+        caption: "⫷𝙎𝘾𝘼𝙉 𝙒𝙄𝙏𝙃𝙄𝙉 8 𝙈𝙄𝙉𝙐𝙏𝙀𝙎⫸",
       });
     }
     return await message.reply("⫷𝘽𝘼𝘿𝙇𝙔 𝙔𝙊𝙐𝙍 𝙎𝙀𝙎𝙎𝙄𝙊𝙉 𝙃𝘼𝙎 𝙀𝙓𝙋𝙄𝙍𝙀𝘿 𝙍𝙀𝙉𝙀𝙒 𝙄𝙏⫸");
