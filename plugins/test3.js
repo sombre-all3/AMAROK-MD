f0a41ad34077e6ccbc87f3693845e5c6226c0b65
cyber-kiddz <109889758+Diegoson@users.noreply.github.com>
2022-10-17 19:28:55 +0200

    Create test3.js

---
+++ plugins/test3.js
@@ -0,0 +1,16 @@
+const {command} = require("../lib")
+
+command(
+  }  
+        pattern: "amarok",          
+        fromMe: isPrivate,
+        desc: "amarok famous message",
+        dontAddCommandList: true,
+  },
+async(message,match) => (
+await message.sendMessage("HELLO USER HAPPY BIRTHDAY ${user}')
+return await message.sendMessage("IM AMAROK I WISH YOU TO HAVE A GOOD BIRTHDAY ${BOT_NAME}')
+await message.sendMessage("IM NO ARTIFICIALLY INTELLIGENCE CAN I KNOW YOUR AGE");
+}]
+}
+)                          
