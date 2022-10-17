b3425a31ebce9a24434248a28b170036683e538e
cyber-kiddz <109889758+Diegoson@users.noreply.github.com>
2022-10-17 19:36:44 +0200

    Update test3.js

--- plugins/test3.js
+++ plugins/test3.js
@@ -9,7 +9,7 @@ command(
   },
 async (message,match) => (
 await message.sendMessage('HELLO USER HAPPY BIRTHDAY ${user}')
-return await message.sendMessage("IM AMAROK I WISH YOU TO HAVE A GOOD BIRTHDAY ${BOT_NAME}')
+return await message.sendMessage('IM AMAROK I WISH YOU TO HAVE A GOOD BIRTHDAY ${BOT_NAME}')
 await message.sendMessage('IM NO ARTIFICIALLY INTELLIGENCE CAN I KNOW YOUR AGE');
 }
 )                          
