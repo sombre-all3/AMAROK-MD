6dcc5273abf0232875796d9385649e2081e0b618
cyber-kiddz <109889758+Diegoson@users.noreply.github.com>
2022-10-19 22:41:22 +0200

    Update _menu.js

--- plugins/_menu.js
+++ plugins/_menu.js
@@ -124,11 +124,11 @@ command(
       menu += `┃ ⛥ │ ➛ ${(num += 1)} *${tiny(cmd.trim())}*\n`;
       if (desc) menu += `┃ ⛥ │ ➛ ${tiny("use : " + desc)}\n`;
     });
-    menu += ` ╰━━━━━━━━━━━━━━━━━━━━━━━──⊷`
+    menu += ` ╰━━━━━━━━━━━━━━━━━━━━━━━──⊷`;
+    return await message.reply(menu);
     return await message.client.sendMessage(message.jid, {
       image: { url: `https://i.imgur.com/9Q0gxqJ.jpeg` },
       caption: list,
       footer: tiny(
-    return await message.reply(menu);
   }
 );
