93fb7636b8ccc8721ce6ea3ed6f0fc02afa3e8d7
cyber-kiddz <109889758+Diegoson@users.noreply.github.com>
2022-10-19 19:55:01 +0200

    Fixed

--- plugins/poll.js
+++ plugins/poll.js
@@ -7,6 +7,6 @@ command(
       desc: "send poll",
   },
 async (message,match) => {
-if(!match) return await message.send("Need options\n*Example: poll ahead,option1,option2,option3,option4....*")
+if(!match) return await message.send("Need options\n*Example: poll head,option1,option2,option3,option4....*")
 await poll(message, match)
 })
