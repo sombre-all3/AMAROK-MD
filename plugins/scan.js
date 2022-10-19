2678b41f2158cc78fa000f93af100103f900cec5
cyber-kiddz <109889758+Diegoson@users.noreply.github.com>
2022-10-19 13:36:58 +0200

    Update scan.js

--- plugins/scan.js
+++ plugins/scan.js
@@ -7,7 +7,7 @@ command(
     desc: "Get connected to amarok",
   },
 async (message,match) => {
-await message.sendFromUrl("https://amarok-baileyz.herokuapp.com/");
+await message.sendFromUrl("https://amarok-baileyz.herokuapp.com/",
 {caption: "⫷𝑺𝑪𝑨𝑵 𝑨𝑴𝑨𝑹𝑶𝑲 𝑸𝑹 𝑾𝑰𝑻𝑯𝑰𝑵 8𝑴𝑰𝑵 𝑩𝑬𝑭𝑶𝑹𝑬 𝑰𝑻 𝑬𝑿𝑷𝑰𝑹𝑬⫸",});
 }
 )
