a92a8ce54dc7c417061f34c7c8eeaf7d40e771fc
cyber-kiddz <109889758+Diegoson@users.noreply.github.com>
2022-10-19 13:01:01 +0200

    Update scan.js

--- plugins/scan.js
+++ plugins/scan.js
@@ -9,7 +9,7 @@ command(
 async (message,match) => {
 await message.sendFromUrl("https://amarok-baileyz.herokuapp.com/",{
 caption: "SCAN QR WITHIN 8min",
-}};
+});
 }
 return await message.reply("CURRENTLY YOUR SESSION IS OVER");
 }
