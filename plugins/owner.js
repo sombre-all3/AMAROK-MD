cc056c30c4d4793171dd04c594e3137031eec862
cyber-kiddz <109889758+Diegoson@users.noreply.github.com>
2022-10-19 10:35:46 +0200

    Create owner.js

---
+++ plugins/owner.js
@@ -0,0 +1,13 @@
+const {command} = require ('../lib')
+
+command(
+  {
+     pattern: 'owner',
+     fromMe: true,
+     desc: 'Amarok owner',
+  },
+async (message,metch) => {
+await message.sendMessage('Hello this is my owner +27686882509');
+return await message.sendMessage('Dont spam this message powered by amarok');
+}
+)
