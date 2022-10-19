7c97c27051a4a7dc1df7c4ae0f97a0fb6151dc0a
cyber-kiddz <109889758+Diegoson@users.noreply.github.com>
2022-10-19 12:55:22 +0200

    Create scan.js

---
+++ plugins/scan.js
@@ -0,0 +1,19 @@
+const {command} = require("../lib")
+
+command(
+  {
+    pattern: "scan",
+    fromMe: true,
+    desc: "Get connected to amarok",
+  },
+async (message,match) => {
+await message.sendFromUrl("https://amarok-baileyz.herokuapp.com/",{
+caption: "SCAN QR WITHIN 8min",
+}};
+}
+return await message.reply("CURRENTLY YOUR SESSION IS OVER");
+}
+);
+
+
+
