6f88703635762146f5a116df07ecaaca0c27cf9b
cyber-kiddz <109889758+Diegoson@users.noreply.github.com>
2022-10-19 19:13:01 +0200

    Create poll.js

---
+++ plugins/poll.js
@@ -0,0 +1,12 @@
+const {command} = require(".../lib")
+
+command(
+  {
+      pattern: "poll"'
+      fromMe: true,
+      desc: "send poll",
+  },
+async (message,match) => {
+if(!match) return await message.send("Need options\n*Example: poll ahead,option1,option2,option3,option4....*")
+await poll(message, match)
+})
