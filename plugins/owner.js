a366bcff9d9259de64a3d29e5e048aaf68afdb17
cyber-kiddz <109889758+Diegoson@users.noreply.github.com>
2022-10-19 08:08:44 +0200

    Update owner.js

--- plugins/owner.js
+++ plugins/owner.js
@@ -1,12 +1,1 @@
-conts {command} = require("../lib");
-command(
-  {
-     pattern: "owner",
-     fromMe: true,
-     desc: "Amarok owner",
-  },
-  async (message,match) => {
-   await message.sendMessage("Hello this is my owner +27686882509");
-   return await message.sendMessage("Dont spam this message powered by amarok");
-  }
-)
+
