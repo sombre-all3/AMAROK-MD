014082dcbaccce05d6aa1e173d83cee72401b888
TOXIC-KICHUX <99080094+TOXIC-KICHUX@users.noreply.github.com>
2022-10-20 17:24:28 +0530

    Updates!

--- config.js
+++ config.js
@@ -13,6 +13,8 @@ module.exports = {
   ANTILINK: toBool(process.env.ANTI_LINK) || false,
   SESSION_ID:process.env.SESSION_ID || "MWhoSnNSclY=",
   ANTILINK_ACTION: process.env.ANTI_LINK || "kick",
+  MENU_THUMB: process.env.MENU_THUMB || "https://i.imgur.com/9Q0gxqJ.jpeg",
+  MENU_TYPE: process.env.MENU_TYPE || "image", 
   ALIVE: process.env.ALIVE || "https://i.imgur.com/c9CNgT5.jpeg",
   LANG: process.env.LANG || "EN",
   AUDIO_DATA: process.env.AUDIO_DATA || '𝘈𝘮𝘢𝘳𝘰𝘬,   𝘊𝘺𝘣𝘦𝘳𝘹𝘬𝘪𝘥,https://i.imgur.com/c9CNgT5.jpeg',
