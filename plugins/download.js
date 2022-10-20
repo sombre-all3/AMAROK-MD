70a5fc92b59b05f130f9c3f6e1c23a7e22b997d8
cyber-kiddz <109889758+Diegoson@users.noreply.github.com>
2022-10-20 07:47:28 +0200

    Update download.js

--- plugins/download.js
+++ plugins/download.js
@@ -10,5 +10,30 @@ command(
    if(!message.reply_message)
    return await message.reply("Enter song name or give me video")
    await toSong(message)
- }
-);
+match = match || message.reply_message.text;
+    if (ytIdRegex.test(match)) {
+      yta(match.trim()).then(async ({ dl_link, title, thumb }) => {
+        let buff = await AddMp3Meta(dl_link, thumb, {
+          title,
+        });
+        message.sendMessage(
+          buff,
+          { mimetype: "audio/mpeg", quoted: message.data },
+          "audio"
+        );
+      });
+    }
+    search(match + "song").then(async ({ videos }) => {
+      await message.reply(`_Downloading ${videos[0].title}_`);
+      yta(videos[0].url).then(async ({ dl_link, title, thumb }) => {
+        let buff = await AddMp3Meta(dl_link, thumb, {
+          title,
+          artist: [videos[0].author],
+        });
+        message.sendMessage(
+          buff,
+          { mimetype: "audio/mpeg", quoted: message.data },
+          "audio"
+  }
+
+)
