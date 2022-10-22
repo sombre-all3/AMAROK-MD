bf796d6e605158a767a930df2f0ca0401d4a0724
cyber-kiddz <109889758+Diegoson@users.noreply.github.com>
2022-10-22 23:04:58 +0200

    Create github.js

---
+++ plugins/github.js
@@ -0,0 +1,14 @@
+const { command, isPublic } = require("../lib")
+command(
+  {
+    pattern: "git",
+    fromMe: isPublic,
+    desc: "to check the bot status",
+    
+  },
+  async (message,match) => {
+    await message.sendMessage("Https://Diegoson/AMAROK-MD");
+    return await message.sendMessage("Thanks for choosing Amarok");
+  };
+ }
+);
