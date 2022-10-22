7d1002830cbcd76a47e73d596d3a51d04c05f211
cyber-kiddz <109889758+Diegoson@users.noreply.github.com>
2022-10-22 22:29:06 +0200

    Create invite.js

---
+++ plugins/invite.js
@@ -0,0 +1,15 @@
+const {command} = require("../lib")
+
+command({ pattern: ["link","invite"], sucReact: "🔗", category: ["group", "all"], },
+  async (message, client) => {
+    if (!message.client.isCreator) { global.catchError = true; return await client.sendMessage( message.from, { text: errorMessage(config.reply.owner) }, { quoted: message } ); };
+    if (!message.isGroup) { global.catchError = true; return await client.sendMessage( message.from, { text: errorMessage(config.reply.group) }, { quoted: message } ); };
+    try {
+        const code = await client.groupInviteCode(message.from);
+        await client.sendMessage( message.from, { text: `🔗 Group Link: https://chat.whatsapp.com/${code}` }, { quoted: message } );
+        global.catchError = false;
+    }  catch (err) {
+        global.catchError = true
+        await client.sendErrorMessage( message.from, err, message.key, message );
+    };
+  }
