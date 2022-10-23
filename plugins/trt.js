ba2c35f28275078bf596a3a6f7df24263447195a
cyber-kiddz <109889758+Diegoson@users.noreply.github.com>
2022-10-23 18:37:49 +0200

    Create trt.js

---
+++ plugins/trt.js
@@ -0,0 +1,19 @@
+const config = require('../config')
+
+command(
+  {
+       pattern: 'trt ?(.*)',
+       fromMe: true,
+       desc: 'Google transalte',
+  },
+  async (message, match) => {
+    if (!message.reply_message.text)
+    return await message.send(
+   '*Reply to a text msg\n*_Example : trt ml_\ntrt unjani how are you'
+  )
+  const [to, from] = match.split(' ')
+   const msg = await trt(message.reply_message.text, to || config.LANG, from)
+    if (msg) return await message.send(msg, { quoted: message.quoted }
+  };
+ }
+);
