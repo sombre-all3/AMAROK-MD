393a13db06845eddc1ec9e244ebf593560fa0281
TOXIC-KICHUX <99080094+TOXIC-KICHUX@users.noreply.github.com>
2022-10-23 17:59:34 +0530

    Fix!!!

--- plugins/_menu.js
+++ plugins/_menu.js
@@ -83,11 +83,11 @@ let comad = cmnd.filter(({ type }) => type == cmmd);
         `Amarok Md\nVersion : ${require("../package.json").version}` ),
       buttons: [
         {
-          buttonId: '.arise',
+          buttonId: prefix + "arise",
           buttonText: { displayText: tiny("⫷ALIVE⫸") },
         },
         {
-          buttonId: '.list',
+          buttonId: prefix + "list",
           buttonText: { displayText: tiny("⫷LIST⫸") },
         },
       ],
