74c2e5e6242c3d6f1fd20cb6d63bc28a1437cd66
Tshephang <100064063+TSH3PHANG@users.noreply.github.com>
2022-10-25 18:36:28 +0200

    Fek 😂

--- plugins/heroku.js
+++ plugins/heroku.js
@@ -6,10 +6,23 @@ const Config = require("../config");
 const heroku = new Heroku({ token: Config.HEROKU_API_KEY });
 const baseURI = "/apps/" + Config.HEROKU_APP_NAME;
 const simpleGit = require("simple-git");
-const { secondsToDHMS } = require("../lib");
 const git = simpleGit();
 const exec = require("child_process").exec;
 const { SUDO } = require("../config");
+
+function secondsToDhms(seconds) {
+        seconds = Number(seconds);
+        var h = Math.floor(seconds % (3600*24) / 3600);
+        var m = Math.floor(seconds % 3600 / 60);
+        var s = Math.floor(seconds % 60);
+        
+      
+        var hDisplay = h > 0 ? h + (h == 1 ? " hour, " : " hours, ") : "";
+        var mDisplay = m > 0 ? m + (m == 1 ? " minute, " : " minutes, ") : "";
+        var sDisplay = s > 0 ? s + (s == 1 ? " second" : " seconds") : "";
+        return  hDisplay + mDisplay + sDisplay;
+        }
+
 
 
 Module(
