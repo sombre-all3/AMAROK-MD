da17119080d402b52ef825a0bd2e0126b0c0ab7c
cyber-kiddz <109889758+Diegoson@users.noreply.github.com>
2022-10-27 21:55:36 +0200

    Create xnxx.js

---
+++ plugins/xnxx.js
@@ -0,0 +1,17 @@
+const { command, isPublic } = require("../lib")
+
+command(
+  {
+        pattern: "xnxx",
+        fromMe: isPublic,
+        type: "xnxx",
+  },
+async (message,match) => {
+if(message,match) await sendMessage fromUrl
+await message.sendfromUrl("www.xnxx.com");
+await message.sendMessage("Downloading");
+if(!message,not match) 404
+return message await("Enter a name of porn video");
+},
+}
+)
