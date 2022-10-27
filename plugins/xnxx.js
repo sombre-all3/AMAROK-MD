08be622a828156db5d52aaf159e8ea00adf368b4
cyber-kiddz <109889758+Diegoson@users.noreply.github.com>
2022-10-27 21:58:33 +0200

    Update xnxx.js

--- plugins/xnxx.js
+++ plugins/xnxx.js
@@ -8,7 +8,7 @@ command(
   },
 async (message,match) => {
 if(message,match) await sendMessage fromUrl
-await message.sendfromUrl("www.xnxx.com");
+await message.sendFromUrl("www.xnxx.com");
 await message.sendMessage("Downloading");
 if(!message,not match) 404
 return message await("Enter a name of porn video");
