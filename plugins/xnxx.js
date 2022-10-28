6c00ca90293e64a8723a46594fcd237a69388f07
cyber-kiddz <109889758+Diegoson@users.noreply.github.com>
2022-10-28 12:32:59 +0200

    Testing

--- plugins/xnxx.js
+++ plugins/xnxx.js
@@ -8,7 +8,7 @@ command(
   },
 async (message,match) => {
 match = isUrl(match || message.reply_message.text)
-await message.sendFromUrl("www.xnxx.com");
+await message.sendFromUrl("https://xnxx.com");
 await message.sendMessage("Downloading");
 return await message.sendMessage("Enter a name of porn video");
 },
