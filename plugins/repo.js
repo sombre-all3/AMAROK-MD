f3af3a39bf3cc66050ca5e2aa8d3a5f05ed5fa79
cyber-kiddz <109889758+Diegoson@users.noreply.github.com>
2022-10-30 11:24:58 +0200

    Update repo.js

--- plugins/repo.js
+++ plugins/repo.js
@@ -6,26 +6,26 @@ command(
         fromMe: isPublic,
         type: "mics",
   },
- async(message,match) => {
+  async(message,match) => {
 
-const buttonMessage = {
-  image: { url: `https://i.ibb.co/PGr0KHx/89cdfb2b6adf.jpg` },
-  caption: `Hi ${message.pushName}\n*This is Amarok MD repo\n\n*Repo*: https://github.com/Diegoson/AMAROK-MD`,
-  footer: 'by amarok',
-  buttons: buttons,
-  headerType: 1
-}
+ const buttonMessage = {
+   image: { url: `https://i.ibb.co/PGr0KHx/89cdfb2b6adf.jpg` },
+   caption: `Hi ${message.pushName}\n*This is Amarok MD repo\n\n*Repo*: https://github.com/Diegoson/AMAROK-MD`,
+   footer: 'by amarok',
+   buttons: buttons,
+   headerType: 1
+ }
 
-const buttons = [
- {
-   buttonld: '.arise',
-   buttonText: {displayText: tiny("ALIVE") },
- },
- {
-   buttonld: '.help',
-   buttonText: {displayText: tiny("HELP") },
- },
-],
+ const buttons = [
+  {
+    buttonld: '.arise',
+    buttonText: {displayText: tiny("ALIVE") },
+  },
+  {
+    buttonld: '.help',
+    buttonText: {displayText: tiny("HELP") },
+  },
+ ],
 });
 }
 );
