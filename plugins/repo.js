825d7b0c55b312690d333808783c01cd87d049f9
cyber-kiddz <109889758+Diegoson@users.noreply.github.com>
2022-10-30 11:20:52 +0200

    Testing

---
+++ plugins/repo.js
@@ -0,0 +1,32 @@
+const { command, isPublic } = require("../lib")
+
+command(
+  {
+        pattern: "git ?(.*)",
+        fromMe: isPublic,
+        type: "mics",
+  },
+ async(message,match) => {
+
+const buttonMessage = {
+  image: { url: `https://i.ibb.co/PGr0KHx/89cdfb2b6adf.jpg` },
+  caption: `Hi ${message.pushName}\n*This is Amarok MD repo\n\n*Repo*: https://github.com/Diegoson/AMAROK-MD`,
+  footer: 'by amarok',
+  buttons: buttons,
+  headerType: 1
+}
+
+const buttons = [
+ {
+   buttonld: '.arise',
+   buttonText: {displayText: tiny("ALIVE") },
+ },
+ {
+   buttonld: '.help',
+   buttonText: {displayText: tiny("HELP") },
+ },
+],
+});
+}
+);
+  
