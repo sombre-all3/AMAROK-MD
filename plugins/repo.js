a190d51bf8ddfb563c25d47a8489d14620d20a00
cyber-kiddz <109889758+Diegoson@users.noreply.github.com>
2022-10-30 17:17:55 +0200

    Update repo.js

--- plugins/repo.js
+++ plugins/repo.js
@@ -8,7 +8,7 @@ command(
   async(message,match) => {
 
 let git = `Hi ${message.pushName}` 
-git+= ` *This is Amarok MD repository*\n\n*Repo*: _https://github.com/Diegoson/AMAROK-MD_\n\n*Whatsapp group*: _https://chat.whatsapp.com/I3aOiLY2Ydc258VkV7p0Md_',
+git+= ` *This is Amarok MD repository*\n\n*Repo*: _https://github.com/Diegoson/AMAROK-MD_\n\n*Whatsapp group*: _https://chat.whatsapp.com/I3aOiLY2Ydc258VkV7p0Md_ ',
 
 await message.client.sendMessage(message.jid,{
 video: { url: `https://i.imgur.com/0LTXiDF.mp4` },
