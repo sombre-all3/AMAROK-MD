92c4c99a1d6d868c20e50d1192e2cf681eb1cc13
cyber-kiddz <109889758+Diegoson@users.noreply.github.com>
2022-10-30 07:37:50 +0200

    Testing

---
+++ plugins/intro.js
@@ -0,0 +1,38 @@
+const { command, isPublic, OWNER_NAME  } = require("../lib/");
+
+command({
+	pattern: 'intro ?(.*)',
+	fromMe: isPublic,
+	type: 'misc',
+}, async (message, match) => {
+
+
+let menu = `╭━━━━━━━━━━━──⊷ 
+┃ ❐ │         *「 MY INTRO 」*
+┃ ❐ │ *BOTNAME* : ${BOT_NAME}
+┃ ❐ │ *OWNER* :${OWNER_NAME}
+┃ ❐ │ *PLACE* : *MADADENI
+┃ ❐ │ *DATE* : ${date}
+┃ ❐ │ *TIME* :${time}
+┃ ❐ │ *GENDER* : *MALE
+┃ ❐ │ *AGE* : *17
+┃ ❐ │ *NUMBER TYPE* :*MOBILE
+┃ ❐ │ *IG ID* :* TOXIC CYBER
+┃ ❐ │
+╰━━━━━━━━━━━──⊷`;
+
+const buttons = [
+  {buttonId: 'id1', buttonText: {displayText: 'I LOVE YOU'}, type: 1},
+]
+
+const buttonMessage = {
+    image: {url: 'https://i.ibb.co/N2rw4k6/2a8b22d25410.jpg'},
+    caption: menu,
+    footer: 'AMAROK',
+    buttons: buttons,
+    headerType: 1
+}
+
+await message.client.sendMessage(message.jid, buttonMessage)
+
+})
