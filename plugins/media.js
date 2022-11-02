03ba3a99ad1070ec18e5324f73c7e3b03c8f367f
cyber-kiddz <109889758+Diegoson@users.noreply.github.com>
2022-11-02 17:31:26 +0200

    Update media.js

--- plugins/media.js
+++ plugins/media.js
@@ -168,29 +168,6 @@ command(
 
 command(
   {
-    pattern: "video ?(.*)",
-    fromMe: isPrivate,
-    type: "downloader",
-  },
-  async (message, match) => {
-    if (!match) return await message.reply("_Send video link_");
-    match = match || message.reply_message.text;
-    if (ytdlRegex.test(match)) {
-      ytdl(match.trim()).then(({ dl_link, title }) => {
-        message.sendFromUrl(dl_link, { filename: title });
-      });
-    }
-    search(match).then(async ({ all }) => {
-      await message.reply(`🎥𝘈𝘔𝘈𝘙𝘖𝘒 𝘐𝘚 𝘋𝘖𝘞𝘕𝘓𝘖𝘈𝘋𝘐𝘕𝘎 𝘠𝘖𝘜𝘙 𝘝𝘐𝘋𝘌𝘖🎥 ${all[0].title}_`);
-      ytdl(all[0].url).then(({ dl_link, title }) => {
-        message.sendFromUrl(dl_link, { filename: title, quoted: message });
-      });
-    });
-  }
-);
-
-command(
-  {
     pattern: "mp3 ?(.*)",
     fromMe: isPrivate,
     type: "downloader",
