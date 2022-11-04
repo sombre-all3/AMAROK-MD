36537e110b267bcb6615b412b3fdf63508cc15f6
TOXIC-KICHUX <99080094+TOXIC-KICHUX@users.noreply.github.com>
2022-11-04 19:59:21 +0530

    Fix!!!

--- plugins/alive.js
+++ plugins/alive.js
@@ -6,9 +6,9 @@ const {
   clockString,
 } = require("../lib");
 var config = require("../config");
-const prefix = require ("../lib");
+const prefix = config.PREFIX
 const { FancyRandom } = require('abu-bot');
-const { OWNER_NAME, BOT_NAME, PREFIX } = require("../config");
+const { OWNER_NAME, BOT_NAME, HANDLERS } = require("../config");
 const { hostname, uptime } = require("os");
 
 command(
@@ -28,7 +28,7 @@ alive+= `
 ┃ ⛥  *BOTNAME* : ${BOT_NAME}
 ┃ ⛥  *USER* : ${message.pushName}
 ┃ ⛥  *OWNER* :  ${OWNER_NAME}
-┃ ⛥  *MY PREFIX* : ${prefix}
+┃ ⛥  *MY PREFIX* : ${HANDLERS}
 ┃ ⛥  *CURRENT DATE* : ${date}
 ┃ ⛥  *TIME* : ${time}
 ┃ ⛥  *I've been up for* : ${clockString(uptime())} 
