1f86afac8079c8990a24435c961f18475ada22b6
cyber-kiddz <109889758+Diegoson@users.noreply.github.com>
2022-11-04 21:59:55 +0200

    Update _leave.js

--- plugins/_leave.js
+++ plugins/_leave.js
@@ -7,7 +7,7 @@ command(
          type: "mics",
   },
   async(message,match) => {
-if(!match) await sock.groupLeave("abcd-xyz@g.us")
+if(!match) await sock.groupLeave("abcd-xyz@g.us");
 await message.reply `${message.pushName} left the group`;
 if(!match) await message.sendMessage("_This command is for group only_");
 },
