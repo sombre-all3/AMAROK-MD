6a890a0322feffbe1a774d4ffaf7d6d72d3d6910
TOXIC-KICHUX <99080094+TOXIC-KICHUX@users.noreply.github.com>
2022-11-04 18:37:10 +0530

    Fix!!!

--- plugins/_menu.js
+++ plugins/_menu.js
@@ -9,7 +9,7 @@ const {
 const speed = require('performance-now');
 const os = require('os');
 const config = require("../config.js");
-const prefix = Config.PREFIX
+const prefix = config.PREFIX
 const { FancyRandom } = require('abu-bot');
 const { OWNER_NAME, BOT_NAME, WORK_TYPE } = require("../config");
 const { hostname, uptime } = require("os");
