214853f58bb40dd3de403b71b92f332c2ac7426a
TOXIC-KICHUX <99080094+TOXIC-KICHUX@users.noreply.github.com>
2022-11-07 17:04:40 +0530

    Fix!!!

--- plugins/_downloader.js
+++ plugins/_downloader.js
@@ -22,7 +22,7 @@ command(
     type: "downloader",
   },
   async (message, match) => {
-    await message.reply("*_ config.SONGDWNLD _*");
+    await message.reply("*_ ${SONGDWNLD} _*");
     match = match || message.reply_message.text;
     if (!match) return await message.reply("_Enter Song Name_");
     //fn
