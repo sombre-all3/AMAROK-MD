acb9e7a8bbc72748147fea34316da6f97bf084af
cyber-kiddz <109889758+Diegoson@users.noreply.github.com>
2022-11-10 18:56:54 +0200

    Update repo.js

--- plugins/repo.js
+++ plugins/repo.js
@@ -8,7 +8,7 @@ command(
   async(message,match) => {
 let { data } = await axios.get('https://api.github.com/repos/Diegoson/AMAROK-MD')
 let git = `Hi ${message.pushName}` 
-git+= ` *This is Amarok MD repository*\n\n*Total Stars*: ${dat.stargazers_count} stars\n\n*Forks* ${data.forks_count} forks\n\n*Repo*: _https://github.com/Diegoson/AMAROK-MD_\n\n*Whatsapp group*: _https://chat.whatsapp.com/I3aOiLY2Ydc258VkV7p0Md_\n\n*Owner*: _http://wa.me/27686881509_`,
+git+= ` *This is Amarok MD repository*\n\n*Total Stars*: ${dat.stargazers_count}\n\n*Forks* ${data.forks_count}\n\n*Repo*: _https://github.com/Diegoson/AMAROK-MD_\n\n*Whatsapp group*: _https://chat.whatsapp.com/I3aOiLY2Ydc258VkV7p0Md_\n\n*Owner*: _http://wa.me/27686881509_`,
 
 await message.client.sendMessage(message.jid,{
 video: { url: `https://i.imgur.com/0LTXiDF.mp4` },
