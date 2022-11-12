a906343926a6cb345c2caa815b08e2b2014fecc5
cyber-kiddz <109889758+Diegoson@users.noreply.github.com>
2022-11-12 16:00:56 +0200

    Create amarok.js

---
+++ plugins/amarok.js
@@ -0,0 +1,26 @@
+const {
+command,
+isPublic
+} = require("../lib")
+
+command(
+  {       
+          pattern: "slot",
+          fromMe: isPublic,
+          type: "mics",
+  },
+  async(message,match) => {
+
+const buttonMessage = {
+    text: `Datebayo my friend ${message.pushName}\n\nYou claimed ${daily.amount}\n\nCurrentl Time ${time}`,
+    caption: "Amarok copyright",
+    footer: "amarok MD",
+    buttons: buttons,
+    headerType: 2
+}
+await message.client.sendMessage(message.jid, buttonMessage)
+})
+
+
+
+  
