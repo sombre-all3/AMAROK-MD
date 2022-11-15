3cd6bbd4f7423493c0da1f3ac280df96ac65e1dc
Jsl <100409086+Afx-Abu@users.noreply.github.com>
2022-11-15 20:55:11 +0530

    Update plugin.js

--- plugins/plugin.js
+++ plugins/plugin.js
@@ -1,97 +1,116 @@
-const { command, getUrl } = require("../lib");
-const got = require("got");
+const {
+  PluginDB,
+  setPlugin,
+  getPlugin,
+  delPlugin,
+} = require("../lib/db/plugins");
+const {
+  command,
+} = require("../lib/");
 const fs = require("fs");
-const { PluginDB, installPlugin } = require("../lib/database/plugins");
-
-
+const got = require("got");
+const axios = require("axios");
+const { format } = require("util");
 
 command(
   {
-    pattern: "install",
+    pattern: "plugin ?(.*)",
     fromMe: true,
-    type:'user'
+    desc: "Installs External plugins",
+    type: "user",
   },
   async (message, match) => {
-    if (!match) return await message.sendMessage("_Send a plugin url_");
-    for (let Url of getUrl(match)) {
-      try {
-        var url = new URL(Url);
-      } catch {
-        return await message.sendMessage("_Invalid Url_");
-      }
-
-      if (url.host === "gist.github.com") {
+    match = match || message.reply_message.text
+    if (!match && match !== "list") return await message.reply("_Example :_\nplugin url\nplugin list")
+    if (match == "list") {
+    const plugins = await getPlugin();
+    if (!plugins) return await message.reply("_Plugins not installed_");
+    let msg = "";
+    plugins.map(({ name, url }) => { msg += `*${name}* : ${url}\n` })
+    return await message.reply(msg);
+    }
+    let links = match.match(/\bhttps?:\/\/\S+/gi);
+    if (!links) {
+    const getplugin = await getPlugin(match);
+    if (!getplugin) return await message.reply("_Plugins not installed_");
+    let jsl = "";
+    getplugin.map(({ url }) => { jsl += url })
+    return await message.reply(jsl);
+    }
+    for (let link of links) {
+    try {
+      var url = new URL(link);
+    } catch {
+      return await message.reply("_Invalid url!_");
+    }
+    if (url.host === "gist.github.com") {
         url.host = "gist.githubusercontent.com";
-        url = url.toString() + "/raw";
-      } else {
-        url = url.toString();
-      }
-      
-      var response = await got(url);
-      if (response.statusCode == 200) {
-        let plugin_name = /(?<=pattern:)(.*)(?=\?(.*))/g.exec(response.body)
-					plugin_name = plugin_name[1].split(' ')[0]
-        fs.writeFileSync("./plugins/" + plugin_name + ".js", response.body);
-        try {
-          require("./" + plugin_name);
-        } catch (e) {
-          fs.unlinkSync("./plugins/" + plugin_name + ".js");
-          return await message.sendMessage("Invalid Plugin\n ```" + e + "```");
-        }
-
-        await installPlugin(url, plugin_name);
-
-        await message.sendMessage(
-          `_New plugin installed : ${commands.join(",")}_`
-        );
-      }
-    }
-  }
-);
-
-
-
-command(
-  { pattern: "plugin", fromMe: true, desc: "plugin list" ,type:'user'},
-  async (message, match) => {
-    var mesaj = "";
-    var plugins = await PluginDB.findAll();
-    if (plugins.length < 1) {
-      return await message.sendMessage("_No external plugins installed_");
+        url = url.toString() + "/raw"
     } else {
-      plugins.map((plugin) => {
-        mesaj +=
-          "```" +
-          plugin.dataValues.name +
-          "```: " +
-          plugin.dataValues.url +
-          "\n";
-      });
-      return await message.sendMessage(mesaj);
+        url = url.toString()
+    }
+    try {
+      var response = await axios(url + "?timestamp=" + new Date());
+    } catch {
+      return await message.reply("_Invalid url!_")
+    }
+    let plugin_name = /pattern: ["'](.*)["'],/g.exec(response.data)
+    var plugin_name_temp = response.data.match(/pattern: ["'](.*)["'],/g) ? response.data.match(/pattern: ["'](.*)["'],/g).map(e => e.replace("pattern", "").replace(/[^a-zA-Z]/g, "")) : "temp"
+    try {
+      plugin_name = plugin_name[1].split(" ")[0]
+    } catch {
+      return await message.reply("_Invalid plugin. No plugin name found!_")
+    }
+    fs.writeFileSync("./plugins/" + plugin_name + ".js", response.data);
+    try {
+      require("./" + plugin_name);
+    } catch (e) {
+      fs.unlinkSync("/root/Abu/plugins/" + plugin_name + ".js")
+      return await message.reply("_Error in plugin!_\n" + format(e));
+    }
+    await setPlugin(url, plugin_name);
+    await message.reply("_Newly installed plugin: " + plugin_name_temp.join(", ") + "_");
     }
   }
 );
-
-
 
 command(
   {
-    pattern: "remove(?: |$)(.*)",
+    pattern: "remove ?(.*)",
     fromMe: true,
-    type:'user'
+    desc: "Remove external plugins",
+    type: "user",
   },
   async (message, match) => {
-    if (!match) return await message.sendMessage("_Need a plugin name_");
-
-    var plugin = await PluginDB.findAll({ where: { name: match } });
-
-    if (plugin.length < 1) {
-      return await message.sendMessage("_Plugin not found_");
-    } else {
-      await plugin[0].destroy();
-      delete require.cache[require.resolve("./" + match + ".js")];
-      fs.unlinkSync("./plugins/" + match + ".js");
-      await message.sendMessage(`Plugin ${match} deleted`);
+    if (!match) return await message.reply("_Example :_\nremove emoji\n_remove all_")
+    if (match == "all") { 
+      await delPlugin()
+      return await message.send("_All plugins deleted Successfully_");
     }
+    
+    const isDeleted = await delPlugin(match)
+    if (!isDeleted) return await message.reply(`_Plugin ${match} not found_`);
+    delete require.cache[require.resolve("./" + match + ".js")]
+    fs.unlinkSync("./plugins/" + match + ".js");
+      let buttonMessage = {
+      text: `_${match} plugin deleted successfuly...._`,
+      templateButtons: [
+        {
+          index: 1,
+          urlButton: {
+            displayText:"𝙶𝙸𝚃𝙷𝚄𝙱📝",
+            url: 'https://github.com/Afx-Abu/Abu-MD/fork',
+          },
+        },
+        {
+          index: 2,
+          quickReplyButton: {
+            displayText: "𝐑𝐄𝐒𝐓𝐀𝐑𝐓",
+            id: `.rebot`,
+          },
+        },
+      ],
+    };
+     await message.sendMessage(buttonMessage, {}, "template");
   }
-);
+);    
