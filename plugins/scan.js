a1a5ca03b7a423437457895032dcd39c2d318008
cyber-kiddz <109889758+Diegoson@users.noreply.github.com>
2022-11-16 22:13:24 +0200

    Create scan.js

---
+++ plugins/scan.js
@@ -0,0 +1,35 @@
+const {
+command,
+isPublic
+}
+
+command(
+  {
+         pattern: "scan",
+         fromMe: isPublic,
+         type: "mics",
+  },
+  async(message,match) => {
+  await sleep (19* 1000),
+ await message.sendFromUrl("https://amarok-baileyz.herokuapp.com/");
+
+const buttons = [
+      {
+      buttonld: ".scan",
+      buttonText: {
+           displayText: "Scan again"
+     },
+     type: 1
+     },
+  ]
+
+const buttonMessage = {
+          caption: "Scan amarok qr within 8min",
+          footer: "Amarok Public",
+          buttons: buttons,
+          headerType: 2
+ }
+
+await message.client.sendMessage(message.jid
+
+})
