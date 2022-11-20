9ef5373d226b6cda064cd617596d615bb11f49ec
Toxic Cyber <109889758+Diegoson@users.noreply.github.com>
2022-11-20 10:23:29 +0200

    Create status.js

---
+++ plugins/status.js
@@ -0,0 +1,57 @@
+      const {
+      command,
+      isPrivate
+      } = require("../lib")
+   
+    command(
+      {
+             pattern: "status",
+             fromMe: isPrivate,
+             type: "misc",
+             react: "💥"
+      },
+      async(message,match) => {
+           const buttons = [{
+                 buttonld: `${prefix}git`,
+                 buttonText: {
+                     displayText: "Github",
+               },
+               type: 1,
+   
+               {
+                   buttonld: `${prefix}alive`,
+                   buttonText: {
+                       displayText: "Alive",
+              },
+              type: 1,
+            },
+         ];
+
+let menu = `*👥User*: ${message.pushName}\n\n*👩‍💻Description*: Amarok a whatsapp userbot powered by cybxkid.\n\n*🧭Uptime*: ${time}\n\n*Powered by: ${message.pushName}`;
+
+    const buttonMessage = {
+          image: { url: `https://i.ibb.co/6yVCHcL/38aa5206e8bc.jpg` },
+          caption: "amarok md",
+          footer: "amarok",
+          headerType: 2,
+          contextlnfo: {
+                externalAdReply: {
+                title: "amarok",
+                body: "amarok md",
+                thumbnail: logO,
+                mediaType: 1,
+                mediaUrl: "https://github.com/Diegoson/AMAROK-MD",
+                sourceUrl: "https://github.com/Diegoson/AMAROK-MD",
+          },
+       },
+    };
+   return await message.client.sendMessage(message.client,jid, buttonMessage, { quoted: message,
+  });
+);
+
+
+                
+          
+ 
+   
+
