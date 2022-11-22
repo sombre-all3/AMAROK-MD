c493e5855099aadae1afe3698d9dbe0a0e903397
Toxic Cyber <109889758+Diegoson@users.noreply.github.com>
2022-11-22 19:09:06 +0200

    Create rbgm.js

---
+++ plugins/rbgm.js
@@ -0,0 +1,22 @@
+   const {
+   command,
+   remove,
+   isPublic
+  } = require("../lib")
+  const fs = require("fs")
+  const Config = require("../config")
+
+ command(
+   {      pattern: "rbgm",
+          fromMe: isPublic,
+          type: "mics",
+   },	
+
+       async (message,match) => {
+       if(message.quoted.imageMessage){
+       let img = await client.downloadAndSaveMediaMessage(message.quoted.imageMessage)
+       let rmbgimg = await remove(fs.readFileSync(img))
+       // let rmbg = await fs.writeFile('./media/rmbg/isexit.jpg', rmbgimg)
+       await client.sendMessage( message.from, { image : rmbgimg, caption : Config.CAPTION }, { quoted: message })
+       await fs.unlinkSync(img);//return await fs.unlinkSync(rmbg);
+   });
