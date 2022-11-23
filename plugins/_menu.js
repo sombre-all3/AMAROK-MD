33f89c10cc18932e20e587c2232eedd47c12978b
Toxic Cyber <109889758+Diegoson@users.noreply.github.com>
2022-11-23 16:16:35 +0200

    Update _menu.js

--- plugins/_menu.js
+++ plugins/_menu.js
@@ -71,7 +71,7 @@ let comad = cmnd.filter(({ type }) => type == cmmd);
  menu += `\n*⛌ ${cmd.trim()}*`;
       });
 
-    menu += `*⛌\n`
+    menu += `*⛌\n`;
     return await message.client.sendMessage(message.jid, {
       image: { url: `https://i.imgur.com/pOkBPvV.jpeg` },
       caption: tiny(menu),
