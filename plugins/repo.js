bec908d32b7ec9a48beeda628288fb89259be51a
Toxic Cyber <109889758+Diegoson@users.noreply.github.com>
2022-11-23 14:27:12 +0200

    Update repo.js

--- plugins/repo.js
+++ plugins/repo.js
@@ -23,7 +23,7 @@ video: { url: `https://i.imgur.com/0LTXiDF.mp4` },
                     body: "Easy to Use",
                     mediaType: 4,
                     sourceUrl: `https://github.com/Diegoson`,
-                    thumbnail: `https://i.imgur.com/M7FXzlY.jpeg',
+                    thumbnail: `https://i.imgur.com/M7FXzlY.jpeg`,
    
                },
              },
