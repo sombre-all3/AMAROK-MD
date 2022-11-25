3b51f3c79c2ba98b533d5f1317491023c8f686f7
Toxic Cyber <109889758+Diegoson@users.noreply.github.com>
2022-11-25 08:33:17 +0200

    Update amarok.js

--- plugins/amarok.js
+++ plugins/amarok.js
@@ -12,27 +12,23 @@
     },
     async(message,match) => {
  
-   const buttons = [{
-             buttonld: ".list",
-             buttonText: {
-                    displayText: "Menu List",
-            },
-            type: 1
-            },
-            {
-             buttonld: ".ping",
-             buttonText: {
-                    displayText: "Speed Test",
-            },
-            type: 1
-            },
-          ];
-
-    const buttonMessage = {
+          const buttonMessage = {
              image: { url: `https://i.ibb.co/HpwY58b/afa809a9756c.jpg` },
              caption: "Testing Amarok",
              footer: tiny(`menu`),
              headerType: 4,
+             buttons: [{
+                 buttonld: ".ping",
+                 buttonText: {
+                       displayText: "Speed Test",
+               },
+               {
+                 buttonld: ".alive",
+                 buttonText: {
+                        displayText: "Alive Button",
+               },
+              ], 
+                 
           }
      return await message.client.sendMessage(message.jid, buttonMessage)
     })
