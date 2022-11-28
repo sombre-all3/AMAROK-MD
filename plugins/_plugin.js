ac47ae1e33dc19422917b22b9625ad6501598a67
Toxic Cyber <109889758+Diegoson@users.noreply.github.com>
2022-11-28 14:37:52 +0200

    Update _plugin.js

--- plugins/_plugin.js
+++ plugins/_plugin.js
@@ -18,7 +18,7 @@ command(
       var url = new URL(match);
     } catch (e) {
       console.log(e);
-      return await message.sendMessage("_Ready"_);
+      return await message.sendMessage("_Ready_");
     }
 
     if (url.host === "gist.github.com") {
