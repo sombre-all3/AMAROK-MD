6503b808bc973af099b7562e3c912117bc5744dd
Toxic Cyber <109889758+Diegoson@users.noreply.github.com>
2022-11-28 16:37:13 +0200

    Update m-media.js

--- plugins/m-media.js
+++ plugins/m-media.js
@@ -13,7 +13,7 @@ const { hostname, uptime } = require("os");
 
 const image = "https://i.ibb.co/dmn1drG/f51569f1668d.jpg";
 const image_1 = "https://i.ibb.co/dmn1drG/f51569f1668d.jpg";
-const audios = "https://i.imgur.com/NzevpfJ.jpeg";
+const audios = "https://i.ibb.co/6yVCHcL/38aa5206e8bc.jpg";
 
 command
 	(
