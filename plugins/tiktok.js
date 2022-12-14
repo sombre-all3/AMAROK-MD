a1344ec194f56ea8ad778ffd3203a6f163990ce7
Toxic Cyber <109889758+Diegoson@users.noreply.github.com>
2022-12-14 09:59:49 +0200

    Update tiktok.js

--- plugins/tiktok.js
+++ plugins/tiktok.js
@@ -1,14 +1,1 @@
-command(
-    {
-           pattern: "tik",
-           fromMe: isPublic,
-           react: "❤️",
-           type: "user",
-    },
-   async(message,match) => {
-  if(!match) await message.sendMessage("I need Link");
-     let response = await getJson(`https://api.ibengtools.my.id/api/download/tiktok?url=${match}&apikey=ibeng`);
-  
-  let media = await (await fetch(response.result.video))
-     await message.client.sendMessage(message.jid , { video : media })
- };
+
