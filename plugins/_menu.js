b4f9b0a6bb5718de64d37b18060a3852b8f82b65
Toxic Cyber <109889758+Diegoson@users.noreply.github.com>
2022-12-14 08:45:22 +0200

    Update _menu.js

--- plugins/_menu.js
+++ plugins/_menu.js
@@ -1,6 +1,6 @@
 const events = require("../lib/event");
 const { command, isPrivate, tiny, serif_B, clockString } = require("../lib");
-const { OWNER_NAME, BOT_NAME } = require("../config");
+const { OWNER_NAME, BOT_NAME, HANDLERS } = require("../config");
 const { hostname, uptime } = require("os");
 command(
   {
