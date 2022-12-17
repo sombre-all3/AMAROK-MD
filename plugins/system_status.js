5ce35957d3607ebfbfc96ee842fa4e1a2345416e
Toxic Cyber <109889758+Diegoson@users.noreply.github.com>
2022-12-17 16:30:40 +0200

    Create system_status.js

---
+++ plugins/system_status.js
@@ -0,0 +1,17 @@
+const {command, isPublic} = require("../lib")
+     
+     command(
+       {      pattern: "stats",
+              fromMe: isPublic,
+              type: "group",
+       },
+      async(message,match) => {
+      
+     const start = new Date().getTime();
+     await message.sendMessage('```Cheking Status```');
+     const end = new Date().getTime ();
+     const usage = `${
+(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)MB / Math.round(require)('os').totalmem/ 1024 / 1024)}MB`;
+     await message.reply('```🧬AMAROK STATUS🧬```\n\n'+'*📍 Ping :* ```'+ping+'``` \n*📶 Ram Usage :*``` '+usage+'```\n');
+   }
+  );
