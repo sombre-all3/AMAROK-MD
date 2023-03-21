832a7e938d2dece16387a58e44fd04ed01c1b543
TOXIC-KICHUX <99080094+TOXIC-KICHUX@users.noreply.github.com>
2023-03-21 20:52:43 +0530

    Fixed

--- plugins/insta.js
+++ plugins/insta.js
@@ -8,5 +8,5 @@ const { command, isPrivate, isUrl, getJson , getBuffer} = require("../lib")
    async(message,match) => {
   if(!match.includes("www.instagram.com")) await message.sendMessage("I need a valid Instagram Link");
      let response = await getJson(`https://api-viper-x0.vercel.app/api/insta?url=${match}`);
-     await message.client.sendMessage(message.jid , { video : { url: response.media.url } , caption : response.result.decs, mimetype: "video/mp4"} , { quoted : message  })
+     await message.client.sendMessage(message.jid , { video : { url: response.media.url } , mimetype: "video/mp4"} , { quoted : message  })
 });
