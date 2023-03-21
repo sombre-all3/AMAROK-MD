6463e154b71db810cd58acd5a0b9de6f00eebde9
TOXIC-KICHUX <99080094+TOXIC-KICHUX@users.noreply.github.com>
2023-03-21 13:25:01 +0530

    Hm

--- plugins/Ai.js
+++ plugins/Ai.js
@@ -10,7 +10,7 @@ command({
 }, 
 async (message, match) => {
 if (!match) return await message.sendMessage("_need text example ai who is diegoson_");
-var api = await getJson(`https://mfarels.my.id/api/openai?text=${match}`)!
+var api = await getJson(`https://mfarels.my.id/api/openai?text=${match}`)
 
 await message.reply(api.result);
 
