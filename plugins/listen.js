48b3baeb3fdfdc037c6ab27ab5796d829880feb4
TOXIC CHAOS <109889758+Diegoson@users.noreply.github.com>
2023-03-21 20:05:03 +0200

    Create listen.js

---
+++ plugins/listen.js
@@ -0,0 +1,45 @@
+  const {
+     command,
+     isPublic,
+     tiny
+  } = require("../lib")
+
+  command(
+     {     pattern: "menu",
+           fromMe: isPublic,
+           type: "user",
+    },
+   async(message,match) => {
+ 
+  let sections = [
+     {  
+    title: "AMAROK MENU",
+    rows: [
+         {title: "「 DOWNLOAD 」", rowld: `downloads`, description: `☆THIS IS THE DOWNLOAD MENU☆`},
+         {title: "「 GROUP 」", rowld: `groups`, description: `☆GROUPS COMMAND LIST HERE☆`}
+    ]
+ },{
+     title: "BOT COMMANDS",
+     rows: [
+           {title: "「 CONVERTER 」", rowld: `vectors`, description: `☆CONVERTER TO STICKERS`},
+           {title: "「 USER MENU 」", rowld: `users`, description: `☆USER MENU LIST☆`},
+           {title: "「 TOOLS MENU 」", rowld: `toolz`, description: `☆TOOLS MENU LIST☆`},
+           {title: "「 SEARCH MENU 」", rowld: `searchz`, description: `☆SEARCH MENU LIST☆`}
+       ]
+   },
+  ]
+ const listMessage = {
+    text: "「 WELCOME TO MENU 」",
+    footer: "┃┗┯━━━━━━━━━━━━◆
+┠┬┤
+┃│❒✗ 
+┃│❒✗ AMAROK MD BOT
+┃│❒✗ 
+┃╰─────────────◆
+",
+  title: "AMAROK NEW MENU",
+  buttonText: "《TAPE HERE》",
+sections 
+}
+return await message.client.sendMessage(message.jid, listMessage})
+});
