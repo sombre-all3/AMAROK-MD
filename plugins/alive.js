8ea0bec714f313f9dcb08133676b311afb945c16
TOXIC CHAOS <109889758+Diegoson@users.noreply.github.com>
2023-03-21 17:49:40 +0200

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -1,54 +1,83 @@
 
 const {
-  command,
-  isPublic,
-  isPrivate,
-  tiny,
-  clockString,
+    command,
+    isPrivate,
+    tiny,
+    clockString,
 } = require("../lib");
 var config = require("../config");
-const prefix = config.PREFIX
-const { FancyRandom } = require('abu-bot');
-const { OWNER_NAME, BOT_NAME, HANDLERS } = require("../config");
-const { hostname, uptime } = require("os");
+const prefix = config.PREFIX;
+const {
+    OWNER_NAME,
+    BOT_NAME,
+    HANDLERS
+} = require("../config");
+const {
+    hostname,
+    uptime
+} = require("os");
 
 command(
-  {
-    pattern: "alive",
-    fromMe: isPrivate,
-    dontAddCommandList: true,
-  },
-async (message,match) => {
-	
-	let [date, time] = new Date()
-      .toLocaleString("en-IN", { timeZone: "Africa/Johannesburg" })
-      .split(",");
-let alive = `
+    {
+        pattern: "alive",
+        fromMe: isPrivate,
+        dontAddCommandList: true,
+    },
+    async (message, match) => {
+        try {
+            let [date, time] = new Date()
+            .toLocaleString("en-IN", {
+                timeZone: "Africa/Johannesburg"
+            })
+            .split(",");
+            let alive = `
 
- *🚦Botname: ${BOT_NAME}
- *🚦Uptime: ${time}
- *⌚Date: ${date}
- *🕸Version: ${require("../package.json").version}
- *👤Owner: ${OWNER_NAME}
- *🎗Prefix: ${HANDLERS}
- *🏅Useer: ${message.pushName}
- *🚦Working hours: ${clockString (uptime())}
-`;
+            *🚦Botname: ${BOT_NAME}*
+            *🚦Uptime: ${time}*
+            *⌚Date: ${date}*
+            *🕸Version: ${require("../package.json").version}*
+            *👤Owner: ${OWNER_NAME}*
+            *🎗Prefix: ${HANDLERS}*
+            *🏅Useer: ${message.pushName}*
+            *🚦Working hours: ${clockString (uptime())}*
+            `;
 
-await message.client.sendMessage(message.jid,{
-image: { url: `https://i.ibb.co/6yVCHcL/38aa5206e8bc.jpg` },
-      caption: tiny(alive),
-      footer: tiny(`amarok md` ),
-      buttons: [
-        {
-        buttonId: `${prefix}owner`,
-        buttonText: {displayText: tiny("OWNER") },
-        },
-        {
-        buttonId: `${prefix}menu`,
-        buttonText: {displayText: tiny("MENU") },
-        },
-      ],
-    });
-  }
-);
+            await message.client.sendMessage(message.jid, {
+                image: {
+                    url: `https://i.ibb.co/6yVCHcL/38aa5206e8bc.jpg`
+                },
+                caption: tiny(alive),
+                footer: tiny(`amarok md`),
+                buttons: [{
+                    buttonId: `owner`,
+                    buttonText: {
+                        displayText: tiny("OWNER")
+                    },
+                },
+                    {
+                        buttonId: `list`,
+                        buttonText: {
+                            displayText: tiny("MENU")
+                        },
+                    },
+                ],
+                contextlnfo: {
+                    externalAdReply: {
+                        title: "AMAROK-MD",
+                        body: "TOXIC-TEAM",
+                        mediaType: 2,
+                        thumbnail: `https://telegra.ph/file/e1fa1db5368bc7eb40c95.png`,
+                        mediaUrl: 'https://amarok-deploy.vercel.app',
+                        sourceUrl: 'https://amarok-deploy.vercel.app',
+                        showAdAttribution: true
+                    }
+                }
+            }, {
+                quoted: message
+            })
+
+        } catch (error) {
+            console.log(error)
+        }
+
+    })
