f380e84209e371f3c83179bf65a7600cb1ad6fc0
TOXIC CHAOS <109889758+Diegoson@users.noreply.github.com>
2023-03-22 16:11:13 +0200

    Create _indirect.js

---
+++ plugins/_indirect.js
@@ -0,0 +1,54 @@
+const { command, isUrl, isPrivate, findMusic } = require("../lib/");
+const jimp = require("jimp");
+let { unlink } = require("fs/promises");
+const got = require("got");
+const FormData = require("form-data");
+const stream = require("stream");
+const { promisify } = require("util");
+const pipeline = promisify(stream.pipeline);
+const fs = require("fs");
+
+command({
+    pattern: "find ?(.*)",
+    fromMe: true,
+    desc: "find the replied music",
+    type: "tools",
+  },
+  async (message, match, msg) => {
+    if (!message.reply_message)
+      return await message.treply("_Reply to a audio or video_");
+    let buff = await msg.quoted.download();
+    let data = await findMusic(buff);
+    if (!data.status) return message.treply(data);
+
+    let buttonMessage = {
+      text: `Title : ${data.title}            
+🚦ARTIST : ${data.artists}            
+🚦ALBUM : ${data.album}                     
+🚦RELEASE : ${data.release_date}`,
+      templateButtons: [
+        {
+          urlButton: {
+            displayText: "YOUTUBE PLAY",
+            url: data.youtube,
+          },
+        },
+        {
+          index: 1,
+          urlButton: {
+            displayText: "SPOTIFY PLAY",
+            url: data.spotify,
+          },
+        },
+        {
+          index: 2,
+          quickReplyButton: {
+            displayText: "AS SONG,
+            id: `${message.prefix}song ${data.song}`,
+          },
+        },
+      ],
+    };
+    await message.sendMessage(buttonMessage, {}, "template");
+  }
+);
