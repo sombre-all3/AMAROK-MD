675b5692dd64c5d0a18768757f2e630c2e570239
TOXIC-KICHUX <99080094+TOXIC-KICHUX@users.noreply.github.com>
2023-03-25 19:12:05 +0530

    Fine done.

--- plugins/ChatGpT.js
+++ plugins/ChatGpT.js
@@ -1,3 +1,5 @@
+const config = require('./config.js')
+const gptapikey = config.GPTAPIKEY
 const {
   command,
   isPrivate,
@@ -13,6 +15,6 @@ async (message, match) => {
 if (!match) return await message.sendMessage("_need text example ai who is diegoson_");
 let response  = await getJson(`https://api-viper-x0.vercel.app/api/openai?openaiapikey=${gptapikey}&text=${match}`)
 
-await message.reply(response.result);
+await message.reply(response.data.text);
 
 });
