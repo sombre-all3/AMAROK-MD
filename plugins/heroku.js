2dcb9f8d2cbef3da7d8dff4cbe6755e05983a022
TOXIC CYBER <109889758+Diegoson@users.noreply.github.com>
2023-03-25 16:29:18 +0200

    New

--- plugins/heroku.js
+++ plugins/heroku.js
@@ -10,18 +10,6 @@ const git = simpleGit();
 const exec = require("child_process").exec;
 const { SUDO } = require("../config");
 
-const got = require("got");
-const Heroku = require("heroku-client");
-const { command, isPrivate, tiny } = require("../lib/");
-const Config = require("../config");
-const heroku = new Heroku({ token: Config.HEROKU_API_KEY });
-const baseURI = "/apps/" + Config.HEROKU_APP_NAME;
-const simpleGit = require("simple-git");
-const { secondsToDHMS } = require("../lib/");
-const git = simpleGit();
-const exec = require("child_process").exec;
-const { SUDO } = require("../config");
-
 command(
   {       pattern: "restart",
           fromMe: false,
