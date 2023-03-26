c455ac504b84442ad575c627f74409fdc22359a6
TOXIC CYBER <109889758+Diegoson@users.noreply.github.com>
2023-03-26 16:46:39 +0200

    Update repo.js

--- plugins/repo.js
+++ plugins/repo.js
@@ -3,10 +3,7 @@ const axios = require("axios")
 
 command({ pattern: "git", fromMe: isPublic, type: "search",}, async(message,match) => {
 let { data } = await axios.get(`https://api.github.com/repos/Diegoson/AMAROK-MD`)
-const buttons = [
-   {buttonld: `menu`, buttonText: {displayText: tiny('MENU')}, type: 1},
-   {buttonld: `list`, buttonText: {ddisplayText: tiny('LIST')}, type: 1}
-]
+
 const buttonMessage = {
   image: {url: `https://telegra.ph/file/6086f101a36f7fc14bff6.png` },
   caption: tiny(`Amarok\n\n🌟Stars: ${data.stargazers_count}\n\n🚦Forks: ${data.forks_count}\n\n👀Views: ${data.watchers_count}\n\n📑Language: ${data.language}\n\n🌲Branch: ${data.default_branch}\n\n💡Created At: ${data.created_at}\n\n💈Last Update: ${data.updated_at}\n\n👁️Mode: public\n\n🔗Git Link: https://github.com/Diegoson/AMAROK-MD`),
