9996a129d88edf19e5f2f73fa63c55e19193077b
TOXIC CYBER <109889758+Diegoson@users.noreply.github.com>
2023-03-26 12:35:51 +0200

    Update repo.js

--- plugins/repo.js
+++ plugins/repo.js
@@ -1,4 +1,4 @@
-const { command, isPublic } = require("../lib")
+const { command, isPublic, tiny } = require("../lib")
  const axios = require("axios")
 
 command({ pattern: "git", fromMe: isPublic, type: "search",}, async(message,match) => {
@@ -10,7 +10,7 @@ let buttons = [
 let buttonMessage = {
   image: {url: `https://telegra.ph/file/6086f101a36f7fc14bff6.png` },
   caption: tiny(`Amarok\n\n🌟Start: ${data.stargazers_count\n\n🚦Forks: ${data.forks_count}\n\n👀Views: ${data.watchers_count}\n\n📑Language: ${data.language}\n\n🌲Branch: ${data.default_branch}\n\n💡Created At: ${data.created_at}\n\n💈Last Update: ${data.updated_at}\n\n👁️Mode: public\n\n🔗Git Link: https://github.com/Diegoson/AMAROK-MD`),
-  footer: tiny(`team toxic`),
+  footer: (toxic team),
   buttons: buttons,
   headerType: 4
 }
