8a04801a795310620276dfbef3cc3c7f419f3f22
TOXIC CYBER <109889758+Diegoson@users.noreply.github.com>
2023-03-26 11:05:07 +0000

    Update repo.js

--- plugins/repo.js
+++ plugins/repo.js
@@ -3,25 +3,18 @@ const { command, isPublic, tiny } = require("../lib")
 
 command({ pattern: "git", fromMe: isPublic, type: "search",}, async(message,match) => {
 let { data } = await axios.get(`https://api.github.com/repos/Diegoson/AMAROK-MD`)
-let buttons = [
+const buttons = [
    {buttonld: `menu`, buttonText: {displayText: tiny('MENU')}, type: 1},
    {buttonld: `list`, buttonText: {ddisplayText: tiny('LIST')}, type: 1}
 ]
-let buttonMessage = {
+const buttonMessage = {
   image: {url: `https://telegra.ph/file/6086f101a36f7fc14bff6.png` },
   caption: tiny(`Amarok\n\n🌟Stars: ${data.stargazers_count}\n\n🚦Forks: ${data.forks_count}\n\n👀Views: ${data.watchers_count}\n\n📑Language: ${data.language}\n\n🌲Branch: ${data.default_branch}\n\n💡Created At: ${data.created_at}\n\n💈Last Update: ${data.updated_at}\n\n👁️Mode: public\n\n🔗Git Link: https://github.com/Diegoson/AMAROK-MD`),
   footer: tiny(`toxic team`),
   buttons: buttons,
   headerType: 4
 }
-return await message.client.sendMessage(message.jid, buttonMessage)
-let reactionMessage = {
-     react: {
-     text: "😎",
-     key: message.key
-   }
- }
-return await message.client.sendMessage(message.jid, reactionMessage, {
+return await message.client.sendMessage(message.jid, buttonMessage, {
  quoted: message 
  })
 });
