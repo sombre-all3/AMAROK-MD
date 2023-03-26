c270313bc9d970fb6cc2cfba6996250a9fb67068
TOXIC CYBER <109889758+Diegoson@users.noreply.github.com>
2023-03-26 12:30:55 +0200

    Update repo.js

--- plugins/repo.js
+++ plugins/repo.js
@@ -10,7 +10,6 @@ let buttons = [
 let buttonMessage = {
   image: {url: `https://telegra.ph/file/6086f101a36f7fc14bff6.png` },
   caption: tiny(`Amarok\n\n🌟Start: ${data.stargazers_count\n\n🚦Forks: ${data.forks_count}\n\n👀Views: ${data.watchers_count}\n\n📑Language: ${data.language}\n\n🌲Branch: ${data.default_branch}\n\n💡Created At: ${data.created_at}\n\n💈Last Update: ${data.updated_at}\n\n👁️Mode: public\n\n🔗Git Link: https://github.com/Diegoson/AMAROK-MD`),
-  footer: tiny(`amarok md`),  
   footer: tiny(`team toxic`),
   buttons: buttons,
   headerType: 4
