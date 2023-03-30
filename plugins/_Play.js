d1489b688797673595b76b452b52e76a03a4da1d
TOXIC CYBER <109889758+Diegoson@users.noreply.github.com>
2023-03-30 07:40:05 +0200

    Update _Play.js

--- plugins/_Play.js
+++ plugins/_Play.js
@@ -2,30 +2,36 @@
    const yts = require("yt-search")
 
    command(
-     {       pattern: "play",
-             fromMe: isPrivate,
-             type: "downloads",
-    },
-    async(message,match) => {
-    if (!match) return await message.sendMessage("_give song name_");
-    const videos = await yts(match)
-    const text = `
-     ╭──────━•
-│🚦 Title: ${videos.all[0].title}
-│🚦Views : ${videos.all[0].views}
-│🚦 Published : ${videos.all[0].ago}
-│🚦 Channel : ${videos.all[0].author.name}
-┗──────━•\n\n`
- let buttons = [
-  {buttonld: `song${videos.all[0].url}`, buttonText: {displayText: 'DOWNLOAD SONG'}, type: 1},
-  {buttonld: `video${videos.all[0].url}`, buttonText: {displayText:' DOWNLOAD VIDEO'}, type: 1}
- ]
-const buttonMessage = {
-image: {url: `${videos.all[0].image}` },
-caption: text,
-footer: `amarok`,
-buttons: buttons,
-headerType: 2
-}
-return await message.sendMessage(message.jid, buttonMessage)
-})
+  {
+    pattern: "play",
+    fromMe: isPrivate,
+    desc: "Downloading songs",
+    type: "downloader",
+  },
+  async (message, match) => {
+         if (!match) return await message.reply("_ᴋᴀɴᴛɪɴɪ ʙʏ sɪʀᴛʀɪʟʟ_")
+                let yts = require("youtube-yts")
+                let search = await yts(match)
+                let anulay = search.videos[Math.floor(Math.random() * search.videos.length)]
+                let buttons = [
+                    {buttonId: `ytmp3 ${anulay.url}`, buttonText: {displayText: 'ꜱᴏɴɢ🎵'}, type: 1},
+                    {buttonId: `ytv ${anulay.url}`, buttonText: {displayText: 'ᴠɪᴅᴇᴏ🎥'}, type: 1}
+                ]
+                let buttonMessage = {
+                    image: { url: anulay.thumbnail },
+                    caption: `
+𒈒 𝑻𝑰𝑻𝑳𝑬 : ${anulay.title}
+𒈒 𝑰𝑫 : ${anulay.videoId}
+𒈒 𝑫𝑼𝑹𝑨𝑻𝑰𝑶𝑵 : ${anulay.timestamp}
+𒈒 𝑽𝑰𝑬𝑾𝑺 : ${anulay.views}
+𒈒 𝑼𝑷𝑳𝑶𝑨𝑫 𝑨𝑻 : ${anulay.ago}
+𒈒 𝑨𝑼𝑻𝑯𝑶𝑹 : ${anulay.author.name}
+𒈒 𝑪𝑯𝑨𝑵𝑵𝑬𝑳 : ${anulay.author.url}
+𒈒 𝑫𝑬𝑺𝑪𝑹𝑰𝑷𝑻𝑰𝑶𝑵 : ${anulay.description}
+𒈒 𝑼𝑹𝑳 : ${anulay.url}`,
+                    footer: BOT_NAME,
+                    buttons: buttons,
+                    headerType: 1
+                }
+                message.client.sendMessage(message.jid, buttonMessage, { quoted: message });
+  })
