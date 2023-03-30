c4da5bd4459acd72574670715f980b4574651055
TOXIC CYBER <109889758+Diegoson@users.noreply.github.com>
2023-03-30 10:11:31 +0200

    Update _Play.js

--- plugins/_Play.js
+++ plugins/_Play.js
@@ -1,7 +1,7 @@
    const { command, isPrivate, isUrl } = require("../lib")
-   const yts = require("yt-search")
+const yts = require("yt-search")
 
-   command(
+command(
   {
     pattern: "play",
     fromMe: isPrivate,
@@ -9,29 +9,21 @@
     type: "downloader",
   },
   async (message, match) => {
-         if (!match) return await message.reply("_ᴋᴀɴᴛɪɴɪ ʙʏ sɪʀᴛʀɪʟʟ_")
-                let yts = require("youtube-yts")
-                let search = await yts(match)
-                let anulay = search.videos[Math.floor(Math.random() * search.videos.length)]
-                let buttons = [
-                    {buttonId: `ytmp3 ${anulay.url}`, buttonText: {displayText: 'ꜱᴏɴɢ🎵'}, type: 1},
-                    {buttonId: `ytv ${anulay.url}`, buttonText: {displayText: 'ᴠɪᴅᴇᴏ🎥'}, type: 1}
-                ]
-                let buttonMessage = {
-                    image: { url: anulay.thumbnail },
-                    caption: `
-◑ᴛɪᴛʟᴇ  : ${anulay.title}
-◑ᴀɴᴏɴʏᴍᴜs : ${anulay.videoId}
-𒈒 𝑫𝑼𝑹𝑨𝑻𝑰𝑶𝑵 : ${anulay.timestamp}
-◑ᴠɪᴇᴡs : ${anulay.views}
-◑ᴘʟᴜsʜᴇᴅ : ${anulay.ago}
-◑ᴀᴜᴛʜᴏʀ : ${anulay.author.name}
-◑ᴜʀʟ : ${anulay.author.url}
-◑ᴅᴇsᴄʀɪᴘᴛᴜᴏɴ : ${anulay.description}
-◑ᴀ ᴜʀʟ : ${anulay.url}`,
-                    footer: BOT_NAME,
-                    buttons: buttons,
-                    headerType: 4
-                }
-                message.client.sendMessage(message.jid, buttonMessage, { quoted: message });
-  })
+    if (!match) return await message.reply("_ᴋᴀɴᴛɪɴɪ ʙʏ sɪʀᴛʀɪʟʟ_")
+    let yts = require("youtube-yts")
+    let search = await yts(match)
+    let anulay = search.videos[Math.floor(Math.random() * search.videos.length)]
+    let buttons = [
+      {buttonId: "ytmp3", buttonText: {displayText: 'ꜱᴏɴɢ🎵', }, type: 1, url: anulay.url},
+      {buttonId: "ytv", buttonText: {displayText: 'ᴠɪᴅᴇᴏ🎥', }, type: 1, url: anulay.url},
+    ]
+    let buttonMessage = {
+      image: { url: anulay.thumbnail },
+      caption: ◑ᴛɪᴛʟᴇ  : ${anulay.title}n◑ᴀɴᴏɴʏᴍᴜs : ${anulay.videoId}n𒈒 𝑫𝑼𝑹𝑨𝑻𝑰𝑶𝑵 : ${anulay.timestamp}n◑ᴠɪᴇᴡs : ${anulay.views}n◑ᴘʟᴜsʜᴇᴅ : ${anulay.ago}n◑ᴀᴜᴛʜᴏʀ : ${anulay.author.name}n◑ᴜʀʟ : ${anulay.author.url}n◑ᴅᴇsᴄʀɪᴘᴛᴜᴏɴ : ${anulay.description}n◑ᴀ ᴜʀʟ : ${anulay.url},
+      footer: "BOT_NAME",
+      buttons: buttons,
+      headerType: 4
+    }
+    message.client.sendMessage(message.jid, buttonMessage, { quoted: message });
+  }
+);
